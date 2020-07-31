c5c06d47024ecfa5641cf5b3fc01cc7f8dcf12aa
Yan OrlovShatskykh <yan.orlov.shatskykh@gmail.com>
2020-07-31 14:06:09 +0300

    Fixed social referenses in header, added logo, made aos animation instead wow, created preloader

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
-new WOW().init();
+document.addEventListener('DOMContentLoaded', ()=> {
+
+  const preloader = document.querySelector('.preloader');
+
+  setTimeout(() => {
+    
+    preloader.classList.add('preloader_disabled');
+    setTimeout(() => {
+      
+      preloader.style.display = 'none';
+      AOS.init({
+        duration: 1500
+      });
+    }, 1000);
+  }, 2000);
+  
+
+
+
+
+
+// new WOW().init();
 
 var mySwiper = new Swiper('.swiper-container', {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
   pagination: {
     el: '.projects-pagination',
     bulletClass: 'projects-bullet',
@@ -8,3 +34,34 @@ var mySwiper = new Swiper('.swiper-container', {
     clickable: true
   },
 });
+
+// $('.owl-carousel').owlCarousel({
+  // loop:true,
+  // margin:30,
+  // nav:true,
+  // navContainerClass:'arrows',
+  // navClass:['arrows__left', 'arrows__right'],
+  // autoplay:true,
+  // autoplayTimeout:2000,
+  // autoplayHoverPause:true,
+  // autoWidth:true,
+  // items: 2
+  // responsive:{
+  //     0:{
+  //         items:1
+  //     },
+  //     700:{
+  //         items:1
+  //     },
+  //     900:{
+  //         items:1
+  //     },
+  //     1200:{
+  //         items:1
+  //     }
+  // }
+// });
+
+}); 
+
+
